1e31368e63429df1eb0ce1ffc10042fd8ea77087
seki yohei <seki.yohei@jp.fujitsu.com>
2017-09-14 15:46:01 +0900

    キャッシュファイル生成処理の途中（色々中途半端）

--- memo_decorator.py
+++ memo_decorator.py
@@ -1,9 +1,11 @@
 import time
 import pickle
 import os
+import hashlib
 import func_analyzer
 
-path = "sample.pickle"
+memo_dir = "memocache/"
+
 
 # 計算時間を測るストップウォッチ
 class Stopwatch(object):
@@ -23,41 +25,75 @@ class Stopwatch(object):
         self.processing_time = self.end_time - self.start_time
         return self.processing_time
 
+# ファイル名に使用するハッシュ値(str型)を取得する
+def get_hash(*args):
+    hash_num = 0x0
+    for data in args:
+        print(data)
+        hash_num = int(hashlib.md5(data.to_bytes(2, 'big')).hexdigest(), 16) ^ hash_num
+    return str(hex(hash_num))
 
-# 計算結果をキャッシュするデコレータ
-def cache(function):
-    def _cache(*args,**kwargs):
+# メモ化用のデコレータ
+def memo(function):
+    def _memo(*args,**kwargs):
 
-        print(dir(function))
-        print(function.__name__)
-        print(function.__code__.co_names)
+        # print(dir(function))
+        # print(function.__globals__)
+        # print(function.__code__.co_names)
         print(func_analyzer.get_load_globals(function))
-        print(*args)
 
-        result = ''
-        if os.path.isfile(path):
-            with open(path, mode='rb') as f:
-                result = pickle.load(f)
+        function_bytecode = function.__code__.co_code
+
+        # キャッシュのファイル名用に引数のデータのハッシュ値を取得
+        cachefilename_hash = get_hash(*args)
+
+        env_path = memo_dir + function.__name__ + '-env.pickle'
+        cache_path = memo_dir + function.__name__ + '-cache-' + cachefilename_hash + '.pickle'
+
+        # メモ化用のキャッシュを置くディレクトリがなければ作成
+        if not os.path.isdir(memo_dir):
+            os.makedirs(memo_dir)
+
+        cache_result = ''
+        env_result = ''
+        # envファイルがあればenvファイルに更新があるかチェックし、異なればenvファイル更新、キャッシュ更新
+        if os.path.isfile(env_path):
+            with open(env_path, mode='rb') as f:
+                env_result = pickle.load(f)
+            # envファイルが更新されていないければ
+            if(env_result == function_bytecode):
+                with open(cache_path, mode='rb') as f:
+                    cache_result = pickle.load(f)
+            else:
+                with open(env_path, mode='wb') as f:
+                    pickle.dump(function_bytecode, f)
+                with open(cache_path, mode='wb') as f:
+                    cache_result = function(*args,**kwargs)
+                    pickle.dump(cache_result, f)
+        # envファイルがなければenvファイルとキャッシュを作成
         else:
-            with open(path, mode='wb') as f:
-                result = function(n)
-                pickle.dump(result, f)
-        return result
-    return _cache
+            with open(env_path, mode='wb') as f:
+                pickle.dump(function_bytecode, f)
+            with open(cache_path, mode='wb') as f:
+                cache_result = function(*args,**kwargs)
+                pickle.dump(cache_result, f)
+        return cache_result
+    return _memo
 
 globalda = 120
 
-@cache
+@memo
 def memo_test(n):
     total = 0
     for num in range(1, 10000000):
         total = total + num
     return total
 
-@cache
-def memo_test2(n):
+
+@memo
+def memo_test2(n, b, c):
     total = globalda
-    for num in range(1, 100):
+    for num in range(1, 10000000):
         total = total + num
     return total
 
@@ -67,44 +103,15 @@ if __name__ == '__main__':
 
     sw.start()
     result = memo_test(2)
-    print('1 回目: %f 秒' % sw.stop())
+    print('[memo_test] 1 回目: %f 秒' % sw.stop())
     print(result)
 
     sw.start()
     result = memo_test(2)
-    print('2 回目: %f 秒' % sw.stop())
+    print('[memo_test] 2 回目: %f 秒' % sw.stop())
     print(result)
 
-    result = memo_test2(10)
-
-
-def test():
-    taro = {'ta' : 'ababa'}
-    print(taro['ta'])
-
-    with open('sample.pickle', mode='wb') as f:
-        pickle.dump(taro, f)
-
-    with open('sample.pickle', mode='rb') as f:
-        ba = pickle.load(f)
-    
-    print(ba['ta'])
-
-
-
-
-# 計算結果をキャッシュするデコレータ
-def old_cache(function):
-    def _cache(n):
-        if not hasattr(function, 'cache'):
-            # キャッシュを入れる辞書が無ければ作る
-            function.cache = {}
-        result = function.cache.get(n)
-        if not result:
-            # 計算結果が無ければ改めて計算する
-            result = function(n)
-            # 計算結果をキャッシュする
-            function.cache = {n: result}
-        # 計算結果を返す
-        return result
-    return _cache
+    sw.start()
+    result = memo_test2(10, 2, 1)
+    print('[memo_test2] 1 回目: %f 秒' % sw.stop())
+    print(result)
