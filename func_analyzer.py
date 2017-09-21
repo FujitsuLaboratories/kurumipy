50b48d8cffffaa78ba7efcfceac4f99621d7a8ee
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2017-09-21 16:01:38 +0900

    Allow global variables whose name is that of a builtin function

--- func_analyzer.py
+++ func_analyzer.py
@@ -1,82 +1,7 @@
 import dis
 
-builtin_function_names = frozenset([
-    "abs",
-    "all",
-    "any",
-    "ascii",
-    "bin",
-    "bool",
-    "bytearray",
-    "bytes",
-    "callable",
-    "chr",
-    "classmethod",
-    "compile",
-    "complex",
-    "delattr",
-    "dict",
-    "dir",
-    "divmod",
-    "enumerate",
-    "eval",
-    "exec",
-    "filter",
-    "float",
-    "format",
-    "frozenset",
-    "getattr",
-    "globals",
-    "hasattr",
-    "hash",
-    "help",
-    "hex",
-    "id",
-    "input",
-    "int",
-    "isinstance",
-    "issubclass",
-    "iter",
-    "len",
-    "list",
-    "locals",
-    "map",
-    "max",
-    "memoryview",
-    "min",
-    "next",
-    "object",
-    "oct",
-    "open",
-    "ord",
-    "pow",
-    "print",
-    "property",
-    "range",
-    "repr",
-    "reversed",
-    "round",
-    "set",
-    "setattr",
-    "slice",
-    "sorted",
-    "staticmethod",
-    "str",
-    "sum",
-    "super",
-    "tuple",
-    "type",
-    "vars",
-    "zip",
-    "__import__",
-])
-
 op_load_global = dis.opmap["LOAD_GLOBAL"]
 op_load_deref = dis.opmap["LOAD_DEREF"]
-
-def is_builtin_function_name(func_name):
-    'Returns True if func_name is a builtin function name'
-    return func_name in builtin_function_names
 
 def get_load_globals(func):
     '''Get global variables used in given func
@@ -86,7 +11,7 @@ def get_load_globals(func):
         (i.argrepr, func.__globals__[i.argrepr])
         for i in dis.get_instructions(func)
         if i.opcode == op_load_global
-        if not is_builtin_function_name(i.argrepr)
+        if i.argrepr in func.__globals__
     ]
 
 def get_load_deref(func):
--- func_analyzer_test.py
+++ func_analyzer_test.py
@@ -4,6 +4,7 @@ import func_analyzer
 g1 = 1
 g2 = 2
 g3 = 3
+abs = lambda i: i if i >= 0 else -i # hide builtin function
 
 class TestFuncAnalyzer(unittest.TestCase):
     a1 = 1
@@ -31,11 +32,15 @@ class TestFuncAnalyzer(unittest.TestCase):
         self.assertEqual([("TestFuncAnalyzer", TestFuncAnalyzer)], func_analyzer.get_load_globals(attr))
         g1 = 11
         self.assertEqual([("g1", 11), ("g3", 3)], func_analyzer.get_load_globals(get))
+        def hide_builtin_function():
+            return abs(-1)
+        self.assertEqual([("abs", abs)], func_analyzer.get_load_globals(hide_builtin_function))
 
     def test_load_deref(self):
         l1 = 1
         l2 = 2
         l3 = 3
+        abs = lambda i: i if i >= 0 else -i # hide builtin function and global function
         def get():
             t1 = l1
             t3 = l3
@@ -44,4 +49,7 @@ class TestFuncAnalyzer(unittest.TestCase):
         self.assertEqual([("l1", 11), ("l3", 3)], func_analyzer.get_load_deref(get))
         def set():
             l2 = 22 # should not be reported because it is assignment
-        self.assertEqual([], func_analyzer.get_load_globals(set))
+        self.assertEqual([], func_analyzer.get_load_deref(set))
+        def hide_builtin_function():
+            return abs(-1)
+        self.assertEqual([("abs", abs)], func_analyzer.get_load_deref(hide_builtin_function))
