2aededfc43c4af02777a918cac8676a56d66007a
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2017-09-26 15:52:01 +0900

    Add statistics feature
    It counts number of calls, hits, and invalidates

--- memo_decorator_test.py
+++ memo_decorator_test.py
@@ -1,124 +1,125 @@
 import unittest
 import memo_decorator
+import time
 from functools import reduce
+
+def get_stats(f):
+    return (f.calls, f.hits, f.invalidates)
 
 g1 = 1
 
 def gf(x):
     return x
 
-class DecoratedCounter():
-    def __init__(self, name):
-        self.name = name
-        self.v = 0
-
-    @memo_decorator.memo
-    def count(self, n):
-        self.v += 1
-        return (self.v, n, g1)
-
-    def __hash__(self):
-        # Does not include v on purpse
-        # Use hash of name because self instance may share same address
-        return hash(self.name)
-
-class PureFunctions():
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.calls = 0
-
-    @memo_decorator.memo
-    def string_length(self, str):
-        self.calls += 1
-        return len(str)
-
-    @memo_decorator.memo
-    def tuple_hash(self, tuple):
-        self.calls += 1
-        return hash(tuple)
+class GlobalClass():
+    pass
 
 class TestMemoDecorator(unittest.TestCase):
     def test_global_variable_changes(self):
         global g1
-        counter = DecoratedCounter("test_global_variable_changes")
-        r = counter.count(1)
-        self.assertEqual(r, (1, 1, 1))
-        r = counter.count(1)
-        self.assertEqual(r, (1, 1, 1))
-        r = counter.count(2)
-        self.assertEqual(r, (2, 2, 1))
-        r = counter.count(1)
-        self.assertEqual(r, (1, 1, 1))
-        g1 = 2
-        r = counter.count(1)
-        self.assertEqual(r, (3, 1, 2))
-        r = counter.count(2)
-        self.assertEqual(r, (4, 2, 2))
-        g1 = 3
-        r = counter.count(1)
-        self.assertEqual(r, (5, 1, 3))
+        @memo_decorator.memo
+        def f(i):
+            return (i, g1)
+        # to invalidates cache
+        g1 = time.time()
+        f(0)
+        initial_invalidates = f.invalidates
+
         g1 = 1
+        self.assertEqual(f(1), (1, 1))
+        self.assertEqual(get_stats(f), (2, 0, initial_invalidates + 1))
+        self.assertEqual(f(1), (1, 1))
+        self.assertEqual(get_stats(f), (3, 1, initial_invalidates + 1))
+        self.assertEqual(f(2), (2, 1))
+        self.assertEqual(get_stats(f), (4, 1, initial_invalidates + 1))
+        self.assertEqual(f(1), (1, 1))
+        self.assertEqual(get_stats(f), (5, 2, initial_invalidates + 1))
+        g1 = 2
+        self.assertEqual(f(1), (1, 2))
+        self.assertEqual(get_stats(f), (6, 2, initial_invalidates + 2))
+        g1 = 1
+        self.assertEqual(f(1), (1, 1))
+        self.assertEqual(get_stats(f), (7, 2, initial_invalidates + 3))
 
     def test_free_variable_changes(self):
         free = 1
-        class DecoratedCounterFree():
-            def __init__(self):
-                self.v = 0
-            @memo_decorator.memo
-            def count(self, n):
-                self.v += 1
-                return (self.v, n , free)
-            # Do not implement __hash__ on purpse
-        counter = DecoratedCounterFree()
-        r = counter.count(1)
-        self.assertEqual(r, (1, 1, 1))
-        r = counter.count(1)
-        self.assertEqual(r, (1, 1, 1))
-        r = counter.count(2)
-        self.assertEqual(r, (2, 2, 1))
-        r = counter.count(1)
-        self.assertEqual(r, (1, 1, 1))
+        @memo_decorator.memo
+        def f(i):
+            return (i, free)
+
+        # to invalidates cache
+        free = time.time()
+        f(0)
+        initial_invalidates = f.invalidates
+
+        free = 1
+        self.assertEqual(f(1), (1, 1))
+        self.assertEqual(get_stats(f), (2, 0, initial_invalidates + 1))
+        self.assertEqual(f(1), (1, 1))
+        self.assertEqual(get_stats(f), (3, 1, initial_invalidates + 1))
+        self.assertEqual(f(2), (2, 1))
+        self.assertEqual(get_stats(f), (4, 1, initial_invalidates + 1))
+        self.assertEqual(f(1), (1, 1))
+        self.assertEqual(get_stats(f), (5, 2, initial_invalidates + 1))
         free = 2
-        r = counter.count(1)
-        self.assertEqual(r, (3, 1, 2))
-        r = counter.count(2)
-        self.assertEqual(r, (4, 2, 2))
-        r = counter.count(1)
-        self.assertEqual(r, (3, 1, 2))
+        self.assertEqual(f(1), (1, 2))
+        self.assertEqual(get_stats(f), (6, 2, initial_invalidates + 2))
+        free = 1
+        self.assertEqual(f(1), (1, 1))
+        self.assertEqual(get_stats(f), (7, 2, initial_invalidates + 3))
 
     def test_same_method_of_different_instances(self):
-        c1, c2 = DecoratedCounter("test_same_method_of_different_instances 1"), DecoratedCounter("test_same_method_of_different_instances 2")
-        r = c1.count(1)
-        self.assertEqual(r, (1, 1, 1))
-        r = c1.count(2)
-        self.assertEqual(r, (2, 2, 1))
-        r = c2.count(2)
-        self.assertEqual(r, (1, 2, 1))
-        r = c1.count(1)
-        self.assertEqual(r, (1, 1, 1))
+        class C:
+            @memo_decorator.memo
+            def f(self):
+                pass
+        c1, c2 = C(), C()
+        c1.f()
+        self.assertEqual(get_stats(c1.f), (1, 0, 0))
+        c2.f()
+        self.assertEqual(get_stats(c2.f), (2, 0, 0))
+        c1.f()
+        self.assertEqual(get_stats(c1.f), (3, 1, 0))
 
-    def test_equality_of_different_objects(self):
-        p = PureFunctions()
-        self.assertEqual(p.calls, 0)
-        p.string_length("aaa")
-        p.string_length("aaa")
-        p.string_length("".join(["a", "aa"]))
-        p.string_length("bbb")
-        self.assertEqual(p.calls, 2)
+    def test_equality_of_different_string_instances(self):
+        free = time.time()
+        @memo_decorator.memo
+        def strlen(s):
+            dummy = free
+            return len(s)
 
-        p.reset()
-        self.assertEqual(p.calls, 0)
-        p.tuple_hash((1, "aaa"))
-        p.tuple_hash((1, "".join(["a", "aa"])))
-        self.assertEqual(p.calls, 1)
-        p.tuple_hash((2, "aaa"))
-        self.assertEqual(p.calls, 2)
-        p.tuple_hash((2, "bbb"))
-        self.assertEqual(p.calls, 3)
-        p.tuple_hash((1, "aaa"))
-        self.assertEqual(p.calls, 3)
+        strlen("")
+        initial_invalidates = strlen.invalidates
+
+        strlen("aaa")
+        self.assertEqual(get_stats(strlen), (2, 0, initial_invalidates))
+        strlen("aaa")
+        self.assertEqual(get_stats(strlen), (3, 1, initial_invalidates))
+        strlen("".join(["a", "aa"]))
+        self.assertEqual(get_stats(strlen), (4, 2, initial_invalidates))
+        strlen("bbb")
+        self.assertEqual(get_stats(strlen), (5, 2, initial_invalidates))
+
+    def test_equality_of_different_tuple_instances(self):
+        free = time.time()
+        @memo_decorator.memo
+        def tuple_hash(t):
+            dummy = free
+            return hash(t)
+
+        tuple_hash(())
+        initial_invalidates = tuple_hash.invalidates
+
+        tuple_hash((1, "aaa"))
+        self.assertEqual(get_stats(tuple_hash), (2, 0, initial_invalidates))
+        tuple_hash((1, "".join(["a", "aa"])))
+        self.assertEqual(get_stats(tuple_hash), (3, 1, initial_invalidates))
+        tuple_hash((2, "aaa"))
+        self.assertEqual(get_stats(tuple_hash), (4, 1, initial_invalidates))
+        tuple_hash((2, "bbb"))
+        self.assertEqual(get_stats(tuple_hash), (5, 1, initial_invalidates))
+        tuple_hash((1, "aaa"))
+        self.assertEqual(get_stats(tuple_hash), (6, 2, initial_invalidates))
 
     def test_deffensive_against_collision(self):
         @memo_decorator.memo
@@ -197,5 +198,5 @@ class TestMemoDecorator(unittest.TestCase):
     def test_class_usage(self):
         @memo_decorator.memo
         def f(s):
-            return DecoratedCounter.__name__ + s
-        self.assertEqual("DecoratedCounter-1", f("-1"))
+            return GlobalClass.__name__ + s
+        self.assertEqual("GlobalClass-1", f("-1"))
