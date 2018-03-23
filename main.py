7cac99e180f0f883a0519cb9301b3272daf737e8
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2018-03-23 10:01:35 +0900

    Rewrite stopwatch because it was copied from a blog post

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 import time
+from contextlib import contextmanager
 import memoization.memo_decorator as memo_decorator
 import threading
 
-# 計算時間を測るストップウォッチ（下記URLのコードのコピペ）
-# http://momijiame.tumblr.com/post/62619539929/python-%E3%81%A7%E3%83%87%E3%82%B3%E3%83%AC%E3%83%BC%E3%82%BF%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E9%96%A2%E6%95%B0%E3%81%AE%E8%A8%88%E7%AE%97%E7%B5%90%E6%9E%9C%E3%82%92%E3%82%AD%E3%83%A3%E3%83%83%E3%82%B7%E3%83%A5%E3%81%99%E3%82%8B
-class Stopwatch(object):
-    def __init__(self):
-        self.start_time = None
-        self.end_time = None
-        self.processing_time = None
-
-    # 計測開始
-    def start(self):
-        self.start_time = time.time()
-
-    # 計測終了
-    def stop(self):
-        self.end_time = time.time()
-        self.processing_time = self.end_time - self.start_time
-        return self.processing_time
-
+@contextmanager
+def stopwatch(format_str):
+    """Print execution time. Argument format_str must have '%f' in it."""
+    start_time = time.time()
+    yield
+    end_time = time.time()
+    print(format_str % (end_time - start_time))
 
 globalda = 120
 
@@ -72,32 +62,19 @@ def recursion(n):
     return n + recursion(n-1)
 
 if __name__ == '__main__':
-    sw = Stopwatch()
-    
-    sw.start()
-    result = memo_test(2)
-    print('[memo_test] 1 回目: %f 秒' % sw.stop())
-    print(str(result) + '\n')
+    with stopwatch('[memo_test] 1st: %f sec'):
+        result = memo_test(2)
+        print(result)
 
-    sw.start()
-    result = memo_test(2)
-    print('[memo_test] 2 回目: %f 秒' % sw.stop())
-    print(str(result) + '\n')
+    with stopwatch('[memo_test] 2nd: %f sec'):
+        result = memo_test(2)
+        print(result)
 
-    sw.start()
-    result = memo_test2(1, 2, 3)
-    print('[memo_test2] 1 回目: %f 秒' % sw.stop())
-    print(str(result) + '\n')
-    
-    '''
-    test_args = testArgs('windsurfing-001.csv', 200, 'lowpass')
-    sw.start()
-    result = memo_test2(test_args.filename, 2, 1)
-    print('[memo_test2] 1 回目: %f 秒' % sw.stop())
-    print(str(result) + '\n')
-    '''
+    with stopwatch('[memo_test2] 1st: %f sec'):
+        result = memo_test2(1, 2, 3)
+        print(result)
 
-    # 1プロセス複数スレッド処理   
+    # Test multiple threads in one process
     callers = [Caller(), Caller(), Caller()]
     for t in callers:
         t.start()
@@ -110,5 +87,5 @@ if __name__ == '__main__':
     for t in callers:
        print(t.lastError)
 
-    # 再帰呼び出し
+    # Test recursive function
     recursion(2)
