0db7f4b2ea284416287397a858a993221697461a
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2018-03-23 09:05:49 +0900

    Add docstring for file operation

--- memoization/file_operation.py
+++ memoization/file_operation.py
@@ -2,33 +2,33 @@ import os
 import pickle
 import threading
 
-# ファイル名に使用するハッシュ値(str型)を取得する
 def get_hash(*args):
+    """Return hex string of hash value of args"""
     hash_num = hash(args)
     return hex(hash_num)
 
-# ファイルにデータをシリアライズして保存
 def file_write(path, data):
+    """Serialize data and save to a file"""
     with open(path, mode='wb') as f:
         pickle.dump(data, f)
 
-# ファイルからデータをデシリアライズして読み込み、返す
 def file_read(path):
+    """Deserialize from a file and return an object"""
     with open(path, mode='rb') as f:
         result = pickle.load(f)
     return result 
 
-# ファイルの存在確認をし、ファイルがあればファイルからデータをデシリアライズして読み込み、返す
 def try_file_read(path):
+    """Try to deserialize from a file and return a tuple of boolean and object"""
     if os.path.isfile(path):
         with open(path, mode='rb') as f:
             result = pickle.load(f)
             return True, result
     return False, None
 
-# ディレクトリを作成（1プロセス複数スレッドにおける排他制御に対応）
 DIR_LOCK = threading.Lock()
 def dir_make(dirpath):
+    """Thread-safe version of makedirs"""
     if not os.path.isdir(dirpath):
         with DIR_LOCK:
             if not os.path.isdir(dirpath):
